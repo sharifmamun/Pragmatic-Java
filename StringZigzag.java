b6bd3aabed55423e901771525b34b9116b72a271
S M Al Mamun <sharif.mamun@gmail.com>
2015-11-24 11:47:34 -0800

    fixed indentation

--- StringZigzag.java
+++ StringZigzag.java
@@ -1,30 +1,30 @@
 public class StringZigzag {
-	public String convert(String a, int b) {
-	    if (b==1)
-	        return a;
-      
-      String result = "";
-      String[] s = new String[b];
-      for (int i=0; i<b; i++) {
-          s[i] = "";
-      }
-      
-      int k=0;
-      int diff = 1;
+  public String convert(String a, int b) {
+	  if (b==1)
+      return a;
 
-      for (int i=0; i< a.length(); i++) {
-          s[k] += Character.toString(a.charAt(i));
-          if (i>0 && i%(b-1) ==0) {
-              diff = -diff;
-          }
-          k += diff;
-      }
-      
-      
-      for (int i=0; i<b; i++) {
-          result += s[i];
-      }
+    String result = "";
+    String[] s = new String[b];
+    for (int i=0; i<b; i++) {
+        s[i] = "";
+    }
+    
+    int k=0;
+    int diff = 1;
 
-	    return result;
+    for (int i=0; i< a.length(); i++) {
+      s[k] += Character.toString(a.charAt(i));
+      if (i>0 && i%(b-1) ==0) {
+          diff = -diff;
+      }
+      k += diff;
+    }
+    
+    
+    for (int i=0; i<b; i++) {
+      result += s[i];
+    }
+
+    return result;
 	}
 }
